00d3c02ecba26cb4bc5b1553152e37f853171e44
АнастасияОдинцова <71040624+Anastasiya-Odintsova@users.noreply.github.com>
2021-01-16 10:34:19 +0300

    Одинцова Анастасия Михайловна

---
+++ экзамен.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Car
+{
+public:
+	void start()
+	{
+		cout <<"start" <<endl;
+	}
+	void stop()
+	{
+		cout << "stop" << endl;
+	}
+	int drive(int howlong)
+	{
+		int distance = howlong * 60;
+		cout << "The car drove " << distance << " km" << endl;
+		return distance;
+	}
+
+};
+
+int main()
+{
+	Car bmw;
+	bmw.start();
+	bmw.drive(100);
+	bmw.stop();
+
+}
