89954b56489d3f1997c6708b4a94f30e3c6e99e1
Drago Linus <den.karkysha@outlook.com>
2021-01-16 10:24:16 +0300

    Афонин Денис

--- CourseApp/CourseApp/CourseApp.cpp
+++ CourseApp/CourseApp/CourseApp.cpp
@@ -1,21 +1,33 @@
-// CourseApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
-#include <cmath>
+
+using namespace std;
+
+class fact
+{
+public:
+	static int factorial(int n)
+	{
+		if (n == 0)
+		{
+			return 0;
+		}
+		int p = 1;
+		for (int i = 1; i <= n; i++)
+			p = p * i;
+		return p;
+	}
+};
 
 int main()
 {
-		std::cout << "Exam" << std::endl;
+	int n;
+	cout << "Enter a number X" << "\n";
+	cin >> n;
+	if (n < 0)
+	{
+		cout << "Error";
+		return 0;
+	}
+	cout << fact::factorial(n);
+	return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
