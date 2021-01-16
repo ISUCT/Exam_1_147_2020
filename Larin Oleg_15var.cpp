6e03d0d199c23058604116abbcee734be447c131
Kislota1 <70697570+Kislota1@users.noreply.github.com>
2021-01-16 10:07:46 +0300

    Larin Oleg

---
+++ Larin Oleg_15var.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+class LenghtConverter 
+{
+public:
+    double length;
+    void KilometersToMiles() 
+    {
+        if (this->length >= 0)
+        {
+            cout <<(this -> length / n) << " miles\n";
+        }
+    }
+    void MilesToKilometers() 
+    {
+        if (this->length >= 0)
+        {
+            cout <<(this->length * n) << " kilometers\n";
+        }
+    }
+
+private:
+    const double n = 1.609; 
+};
+
+int main()
+{
+    LenghtConverter One; 
+    cout << "Enter the miles" << endl;
+    cin >> One.length;
+    
+    cout << One.length <<  "  miles = ";
+    One.KilometersToMiles(); 
+
+    LenghtConverter Two; 
+    cout << "Enter the kilometers" << endl;
+    cin >> Two.length;
+    
+    cout << Two.length << " kilometers = ";
+    Two.KilometersToMiles(); 
+}
